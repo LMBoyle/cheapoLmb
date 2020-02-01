94ee754ea3a43ad8ca756da8dca65889ecbeea52
LMBoyle <boylelukem@gmail.com>
2020-02-01 16:13:14 -0500

    updated welcome page

--- client/src/pages/App/Welcome.js
+++ client/src/pages/App/Welcome.js
@@ -7,7 +7,6 @@ import "./Pages.css"
 
 // Components
 import { Col, Row, Container } from "../../components/Grid";
-import Jumbotron from "../../components/Jumbotron";
 import { CardHeader, CardImg, Card } from "../../components/Card"
 
 // Images
@@ -22,64 +21,50 @@ class Welcome extends Component {
 
   render() {
     return (
-      <div className="container my-0">
-        <Card
-          cardClass={"cardWrap"}
-          styleCard={{minHeight: 1028}}
-          styleBody={{padding: "0 0 50px 0"}}
-        >
-          <Row>
-            <Col size="md-12">
-              <Jumbotron
-                style={{color: "#650D1B"}}
-              >
-                <h1> Welcome to Cheapo! </h1>
-                <p> We strive to help you help your friends save money! </p>
-              </Jumbotron>
-            </Col>
-          </Row>
-          <Container>
+      <main className="my-0">
+        <Container>
+
+          <header>
             <Row>
-              <Col size="md-6">
+              <Col size="md-12">
+                We strive to help you help your friends save money! 
+              </Col>
+            </Row>
+          </header>
+
+          <section>
+            <Row>
+              {/* Save Money */}
+              <Col size='md-4'>
                 <Rotate top left>
-                  <CardImg
-                    src={savingJars}
-                    alt="Jars full of change with labels for different saving goals"
-                  />
+                  <img src={savingJars} alt="Jars full of change with labels for different saving goals"/>
                 </Rotate>
+                <h2> Save Money </h2>
+                <p> Save towards rent, vacation or treating yourself to something nice! </p>
               </Col>
-              <Col size="md-6">
-                <CardHeader
-                  title="Save Money"
-                  styleHeader={{backgroundColor: "#BF7E04"}}
-                  styleCard={{textAlign: "center", top: "3rem"}}
-                >
-                  Save towards rent, vacation or treating yourself to something nice!
-                </CardHeader>
+
+              {/* Connect With Friends */}
+              <Col size='md-4'>
+                <Rotate top left>
+                  <img src={friendsMoney} alt="Group of friends reaching for falling money"/>
+                </Rotate>
+                <h2> Connect With Friends </h2>
+                <p> See how close friends are to meeting their goals and cheer them on! </p>
+              </Col>
+
+              {/* Learn Good Habits */}
+              <Col size='md-4'>
+                <Rotate top left>
+                  <img src={friendsMoney} alt="Group of friends reaching for falling money"/>
+                </Rotate>
+                <h2> Learn Good Habits </h2>
+                <p> Create long lasting habits </p>
               </Col>
             </Row>
-            <Row>
-              <Col size="md-6">
-                <CardHeader
-                  title="Connect With Friends"
-                  styleHeader={{backgroundColor: "#BF7E04"}}
-                  styleCard={{textAlign: "center", top: "7rem"}}
-                >
-                  See how close friends are to meeting their goals and cheer them on!
-                </CardHeader>
-              </Col>
-              <Col size="md-6">
-                <Rotate top right>
-                  <CardImg
-                    src={friendsMoney}
-                    alt="Group of friends reaching for falling money"
-                  />
-                </Rotate>
-              </Col>
-            </Row>
+          </section>
+
           </Container>
-        </Card>
-      </div>
+      </main>
     );
   };
 };
